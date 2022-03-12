3b706bcc5eda89b6d6d5928b1ade205e60f28b41
mby444 <bimayudha161@gmail.com>
2022-03-12 10:39:17 +0700

    second

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const { downloader: Downloader } = require("instagram-url-downloader");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Server running at port ${port}...`);
+});
