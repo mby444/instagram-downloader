e42c597cb982f681ca36e442e73fbb0e0d436ab4
mby444 <bimayudha161@gmail.com>
2022-03-23 14:27:53 +0700

    adding loader in main.js client

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,23 @@
 const input = document.querySelector(".input");
 const btnSubmit = document.querySelector(".btn-submit");
 const form = document.querySelector("form");
+const content = document.querySelector("#content");
 
+const spinner = `
+    <div class="loader-conainer container-fluid d-flex justify-content-center align-items-center mt-5 mb-5">
+        <div class="loader spinner-border text-success"></div>
+    </div>
+`;
+
+const isEmpty = (str) => {
+    str = str.trim();
+    return str.length === 0;
+}
+
+form.addEventListener("submit", (event) => {
+    if(isEmpty(input.value)){
+        event.preventDefault();
+        return false;
+    }
+    content.innerHTML = spinner;
+})
